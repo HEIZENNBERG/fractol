5206da82b6f9c057f59650ea8eb150dee4e49201
Oussama Najem <onajem@e3r3p17.1337.ma>
2025-02-15 15:36:51 +0100

    progress

--- fractol.c
+++ fractol.c
@@ -6,7 +6,7 @@
 /*   By: onajem <onajem@student.1337.ma>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/14 16:59:31 by onajem            #+#    #+#             */
-/*   Updated: 2025/02/14 18:13:32 by onajem           ###   ########.fr       */
+/*   Updated: 2025/02/15 15:29:52 by onajem           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,16 @@ void handle_error(void)
 }
 
 
+void event_init(t_fractol *frac)
+{
+	// mlx_hook(frac->win, KeyPress, KeyPressMask, key_handler, frac);
+	// mlx_hook(frac->win, ButtonPress, ButtonPressMask, mouse_handler, frac);
+	// mlx_hook(frac->win, DestroyNotify, StructureNotifyMask, close_handler, frac);
+	// mlx_hook(frac->win, MotionNotify, PointerMotionMask, julia_track, frac);
+}
+
+
+
 void init(t_fractol *fract)
 {
     fract->conn = mlx_init();
@@ -33,14 +43,14 @@ void init(t_fractol *fract)
         free(fract->conn);
         handle_error();
     }
-    fract->img.addr = mlx_new_image(fract->conn, WIDTH, HEIGTH);
-    if (!fract->img.addr)
+    fract->img.img = mlx_new_image(fract->conn, WIDTH, HEIGTH);
+    if (!fract->img.img)
     {
         mlx_destroy_window(fract->conn, fract->win);
         free(fract->conn);
         handle_error();
     }
-    fract->img.addr = mlx_get_data_addr(fract->img.addr, &fract->img.bits_per_pixel, &fract->img.line_len, &fract->img.endian);
+    fract->img.addr = mlx_get_data_addr(fract->img.img, &fract->img.bits_per_pixel, &fract->img.line_len, &fract->img.endian);
 
     // event_init()
     // data_init()
@@ -51,26 +61,57 @@ double map(double orgin, double new_min, double new_max, double old_min, double 
     return (new_max - new_min) * (orgin - old_min) / (old_max - old_min) + new_min;
 }
 
+t_complex sum_c(t_complex z1, t_complex z2)
+{
+    t_complex res;
+    
+    res.x = z1.x + z2.x;
+    res.y = z1.y + z2.y;
+    return (res);
+}
+
+t_complex sqrt_c(t_complex z)
+{
+    t_complex res;
+    
+    res.x = (z.x * z.x) - (z.y * z.y);
+    res.y = 2 * z.x * z.y ;
+    return (res);
+}
+
+void	put_pxl(t_img *data, int x, int y, int color)
+{
+	char	*dst;
+
+	dst = data->addr + (y * data->line_len + x * (data->bits_per_pixel / 8));
+	*(unsigned int*)dst = color;
+}
+
+
 void process_pixel(int x, int y, t_fractol *frac)
 {
     t_complex z;
     t_complex c;
+    int i;
+    int color;
 
     z.x = 0.0;
     z.y = 0.0;
     c.x = map(x, -2, 2, 0, WIDTH);
-    c.y = map(x, 2, -2, 0, WIDTH);
-
-    while ()
+    c.y = map(y, 2, -2, 0, WIDTH);
+    i = 0;
+    while (i < 42)
     {
-        // z = sum_c(sqrt_c(z), c)
-        if ()
+        z = sum_c(sqrt_c(z), c);
+        if ((z.x * z.x) + (z.y * z.y) > 4)
         {
-            put_pxl();
+            color = map(i, BLACK, WHITE, 0, 150);
+            put_pxl(&frac->img, x, y, color);
             return ;
         }
+        i++;
     }
-    
+    put_pxl(&frac->img, x, y, MAGENTA);
 }
     
 
@@ -90,6 +131,7 @@ void render(t_fractol *frac)
         }
         y++;
     }
+    mlx_put_image_to_window(frac->conn, frac->win, frac->img.img, 0, 0);
 }
 
 
@@ -97,7 +139,7 @@ int main(int ac, char *av[])
 {
     t_fractol fract;
     
-    if (ac == 2 && ft_strncmp(av[1], "mandlebrot", 10) || ac == 4 && ft_strncmp(av[1], "julia", 5))
+    if (ac == 2 && !ft_strncmp(av[1], "mandlebrot", 11) || ( ac == 4 && !ft_strncmp(av[1], "julia", 5)))
     {
         fract.name = av[1];
         init(&fract);
@@ -105,7 +147,7 @@ int main(int ac, char *av[])
         render(&fract);
 
         mlx_loop(fract.conn);
-        printf("successs\n");
+
     } 
     else 
     {
--- fractol.h
+++ fractol.h
@@ -6,7 +6,7 @@
 /*   By: onajem <onajem@student.1337.ma>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/14 16:59:35 by onajem            #+#    #+#             */
-/*   Updated: 2025/02/14 17:59:32 by onajem           ###   ########.fr       */
+/*   Updated: 2025/02/15 13:51:14 by onajem           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,20 @@
 #include <mlx.h>
 #define WIDTH 800
 #define HEIGTH 800
+
+#define BLACK   0x000000  // RGB(0, 0, 0)
+#define WHITE   0xFFFFFF  // RGB(255, 255, 255)
+#define RED     0xFF0000  // RGB(255, 0, 0)
+#define GREEN   0x00FF00  // RGB(0, 255, 0)
+#define BLUE    0x0000FF  // RGB(0, 0, 255)
+#define YELLOW  0xFFFF00  // RGB(255, 255, 0)
+#define CYAN    0x00FFFF  // RGB(0, 255, 255)
+#define MAGENTA 0xFF00FF  // RGB(255, 0, 255)
+#define GRAY    0x808080  // RGB(128, 128, 128)
+#define ORANGE  0xFFA500  // RGB(255, 165, 0)
+#define PURPLE  0x800080  // RGB(128, 0, 128)
+#define PINK    0xFFC0CB  // RGB(255, 192, 203)
+
 
 typedef struct s_complex
 {
